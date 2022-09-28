889e1c4b99006a2ac6a19964a7680eb7642113c6
Edwin Leonard Salim <edwin.salim@ukirama.com>
2022-09-28 16:21:11 +0700

    link User and Event through EventAttendee

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :user, foreign_key:  "creator_id", class_name: "User"
+
+  has_many :event_attendees, foreign_key: "event_id", class_name: "EventAttendee"
+  has_many :attendees, through: :event_attendees, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :events
+  has_many :event_attendees, foreign_key: :user_id, class_name: "EventAttendee"
+  has_many :event_attended, through: :event_attendees, source: :event
+  has_many :events, foreign_key: :creator_id, class_name: "Event"
 end
