43729c52a718a9ff9ca70d4d13984e840af2ff9f
Edwin Leonard Salim <edwin.salim@ukirama.com>
2022-09-29 11:34:59 +0700

    Add routing for add and remove attendance

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'events#index'
-  resources :events
+  resources :events do
+    get 'attend', to: 'events#attend_event'
+    get 'cancel-attend', to: 'events#remove_attendance'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
